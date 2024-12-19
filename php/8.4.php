b387491a1c359b40ee7feb5ab9df74cda498c162
koji9412 <koji0412ohta@gmail.com>
2024-12-19 10:55:57 +0900

    fixed

--- php/8.4.php
+++ php/8.4.php
@@ -1,15 +1,2 @@
 <?php
-
-class MyClass {
-    public private(set) string $data = '秘密の情報'; // 型情報が必須
-
-    public function internalSetData($value) {
-        $this->data = $value;
-    }
-}
-
-$instance = new MyClass();
-echo $instance->data; // 秘密の情報
-// $instance->data = '新しい情報'; // PHP Fatal error:  Uncaught Error: Cannot modify private(set) property MyClass::$data from global scope
-$instance->internalSetData('新しい情報'); // 内部的な書き込みは可能
-echo $instance->data; // 新しい情報
+echo 'PHP 8.4' . PHP_EOL;
